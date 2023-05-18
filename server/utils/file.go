80518fe500db2d79356e9c4c5ace624eeb288fec
9bany <bany.y0599@gmail.com>
2023-05-18 17:42:09 +0700

    add: file path walk

---
+++ server/utils/file.go
@@ -0,0 +1,17 @@
+package utils
+
+import (
+	"os"
+	"path/filepath"
+)
+
+func FilePathWalkDir(root string) ([]string, error) {
+	var files []string
+	err := filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
+		if !info.IsDir() {
+			files = append(files, path)
+		}
+		return nil
+	})
+	return files, err
+}
