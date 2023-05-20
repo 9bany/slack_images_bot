99fb422d2f67c88dbe140ebce7b956476fc40c5b
9bany <bany.y0599@gmail.com>
2023-05-20 10:51:38 +0700

    add: script fill image to db

---
+++ scripts/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"context"
+	"database/sql"
+	"log"
+	"os"
+
+	_ "github.com/lib/pq"
+
+	db "github.com/9bany/bot_workflows/src/db/sqlc"
+	"github.com/9bany/bot_workflows/src/utils"
+)
+
+func main() {
+	config := utils.LodConfig(".")
+	conn, err := sql.Open(config.DBDriver, config.DBSource)
+	if err != nil {
+		log.Println("got at error: ", err)
+	}
+
+	querieer := db.New(conn)
+
+	files, err := utils.FilePathWalkDir("./data/images")
+	if err != nil {
+		log.Println("can not file path walk")
+	}
+
+	for index, element := range files {
+		file_data, err := os.ReadFile(element)
+		if err != nil {
+			log.Println("can not open file")
+		}
+
+		_, err = querieer.CreateImage(context.Background(), file_data)
+		if err != nil {
+			log.Println("can not insert image to db")
+		}
+
+		log.Println("Inserted a image to db", index)
+	}
+
+}
